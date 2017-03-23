3fc5cc6aa3e8b2fc7286a9355ef86a9ef689d63e
David Kroll <curtain333-code@yahoo.com>
2017-03-22 20:37:51 -0400

    Practice controller setup, packages

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -4,10 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-
 class PracticeController extends Controller
 {
 
+// PRACTICE METHOD 3: USING rych-random package:
+    public function practice3() {
+
+      // $random = new Rych\Random\Random(); this didn't work, it cause the following error:
+      // FatalThrowableError in PracticeController.php line 15:
+      //Class 'App\Http\Controllers\Rych\Random\Random' not found
+      $random = new \Rych\Random\Random(); // this worked, by pointing to global namespace.  you could
+      // also fix by adding the following statement at the top of this PracticeController.php file:
+      // use Rych\Random\Random;
+      // that would allow you to then say this here in practice3:
+      // $random = new Random();
+
+      // Generate a 16-byte string of random raw data
+      $randomBytes = $random->getRandomBytes(16);
+      dump($randomBytes);
+
+      // Get a random integer between 1 and 100
+      $randomNumber = $random->getRandomInteger(1, 100);
+      dump($randomNumber);
+
+      // Get a random 8-character string using the
+      // character set A-Za-z0-9./
+      $randomString = $random->getRandomString(8);
+      dump($randomString);
+    }
+
+
+
+// PRACTICE METHOD 2:
+//    use the 'config' global helper function to find out the value of APP_DEBUG being pulled in from the
+//    .env file into the app.php file by the line of code 'debug' => env('APP_DEBUG', false),
+      public function practice2() {
+          dump(config('app.debug')); // do dump(config('app')); to see ALL settings
+      }
 
 
 // PRACTICE METHOD 1:
---
+++ config/foobooks.php
@@ -0,0 +1,6 @@
+<?php
+
+
+return [
+  'defaultNumberOfBooks' => 10,
+];
